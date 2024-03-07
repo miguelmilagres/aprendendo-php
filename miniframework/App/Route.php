1b319831f684d229535a7cf108082729f72f8e10
miguelmilagres <miguelmdemacedo@gmail.com>
2024-03-07 11:16:34 -0300

    abstracao routes

--- miniframework/App/Route.php
+++ miniframework/App/Route.php
@@ -2,24 +2,10 @@
 
 namespace App;
 
-class Route {
+use MF\Init\Bootstrap;
 
-    private $routes;
-
-    public function __construct() {
-        $this->initRoutes();
-        $this->run($this->getUrl());
-    }
-
-    public function getRoutes() {
-        return $this->routes;
-    }
-
-    public function setRoutes(array $routes) {
-        $this->routes = $routes;
-    }
-
-    public function initRoutes() {
+class Route extends Bootstrap{
+    protected function initRoutes() {
         $routes['home'] = array(
             'route' => '/',
             'controller' => 'IndexController',
@@ -39,22 +25,6 @@ class Route {
         );
 
         $this->setRoutes($routes);
-    }
-
-    public function run($url) {
-        foreach($this->getRoutes() as $key => $route) {
-            if ($url == $route['route']) {
-                $class = "App\\Controllers\\".$route['controller'];
-
-                $controller = new $class;
-                $action = $route['action'];
-                $controller->$action();
-            }
-        }
-    }
-
-    public function getUrl() {
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 }
 
--- miniframework/public/index.php
+++ miniframework/public/index.php
@@ -3,6 +3,4 @@
     require_once "../vendor/autoload.php";
 
     $route = new \App\Route;
-    echo '<hr>';
-    print_r($route->getUrl());
 ?>
