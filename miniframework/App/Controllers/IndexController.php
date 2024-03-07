725258e159a159cf8b2a2276c69577b5742b67fd
miguelmilagres <miguelmdemacedo@gmail.com>
2024-03-07 16:16:06 -0300

    conexao banco de dados e models

--- miniframework/App/Controllers/IndexController.php
+++ miniframework/App/Controllers/IndexController.php
@@ -3,20 +3,29 @@
 namespace App\Controllers;
 
 use MF\Controller\Action;
+use App\Connection;
+use App\Models\Produto;
 
 class IndexController extends Action {
     public function index() {
-        $this->view->dados = array('PHP', 'JAVA', 'Go');
+        // $this->view->dados = array('PHP', 'JAVA', 'Go');
+        $conn = Connection::getDb();
+        $produto = new Produto($conn);
+        
+        $produto = $produto->getProdutos();
+
+        $this->view->dados = $produto;
+
         $this->render('index', 'layout1');
     }
     
     public function sobreNos() {
-        $this->view->dados = array('MySQL', 'Oracle', 'MariaDB');
+        // $this->view->dados = array('MySQL', 'Oracle', 'MariaDB');
         $this->render('sobreNos', 'layout2');
     }
 
     public function contato() {
-        $this->view->dados = array('Miguel', 'Elisa', 'Rodrigo');
+        // $this->view->dados = array('Miguel', 'Elisa', 'Rodrigo');
         $this->render('contato', 'layout3');
     }
 }
